4f6ec25c616d602094bad82f33e035083834974d
Lalith Shravan Guruprasad <lalithg@outlook.com>
2023-09-11 17:29:20 -0700

    Resetting Workout Buddy - Start of Project

--- Workout Buddy/WorkoutsView.swift
+++ Workout Buddy/WorkoutsView.swift
@@ -7,37 +7,10 @@
 
 import SwiftUI
 
-
-
 struct WorkoutsView: View {
     
-    @State var w1 = ["Legs 1", "Upper 1", "Legs 2", "Upper 2", "Cardio"]
-    
     var body: some View {
-        
-        VStack {
-            
-            NavigationView {
-                
-                List(w1, id: \.self) {item in
-                    
-                    Text(item)
-                    
-                }
-            }
-        
-            HStack {
-                
-                Button("add"){
-                    w1.append("added")
-                }
-                
-                Button("remove"){
-                    w1.removeLast()
-                }
-                
-            }
-        }
+        Text("Hello World")
     }
 }
 
